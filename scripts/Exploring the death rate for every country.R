ade9ea19bc33634f38208e52d5258ec4a07cd24b
Romanos001 <ostapykrop1991@gmail.com>
2023-11-12 20:53:00 +0000

    Included lines for graph and data cleaning

--- scripts/Exploring the death rate for every country.R
+++ scripts/Exploring the death rate for every country.R
@@ -8,6 +8,8 @@ library(plotly)
 
 infant_mortality <- read_csv("data/infant_mortality.csv")
 
+# puts the data in tidy format, cleans names, makes sure that year is treated 
+# as date data and filters four countries of interest
 subset_infant_mortality <- infant_mortality %>%
   pivot_longer(cols="1960":"2020", 
                names_to="year",               
@@ -27,6 +29,45 @@ subset_infant_mortality <- infant_mortality %>%
 # (US,UK, Japan = lowest infant death rates, Afghanistan = highest infant death rates)
 
 
+# ggplot plotting the infant mortality rate by country
+mortality_figure <- ggplot(data = subset_infant_mortality,
+                           aes(x = year,
+                               y = infant_mortality_rate,
+                               color = country_name)) +
+  geom_line() +
+  scale_color_manual(values = c("black", "blue", "magenta", "orange")) +
+  annotate(geom = "text",
+           x = 2023,
+           y = 50,
+           label = "Afghanistan",
+           size = 4,
+           colour="black") +
+  annotate(geom = "text",
+           x = 2023,
+           y = -2,
+           label = "Japan",
+           size = 4,
+           colour="blue") +
+  annotate(geom = "text",
+           x = 2023,
+           y = 15,
+           label = "United Kingdom",
+           size = 4,
+           colour="orange") +
+  annotate(geom = "text",
+           x = 2023,
+           y = 5,
+           label = "United States",
+           size = 4,
+           colour="magenta") +
+  geom_vline(xintercept = 2000,
+             lty = 2) +
+  theme_minimal()+
+  theme(legend.position="none")+
+  xlim(1970, 2025)+
+  labs(x="Year",
+       y="Deaths per 100,000")+
+  ggtitle("Mortality rate, infant (per 1,000 live births) \nhas been steadily falling in Afghanistan from 1970 to present")
 
 
-
+ggsave("figures/infant mortality graph.png", plot = mortality_figure, dpi=900, width = 7, height = 7)
